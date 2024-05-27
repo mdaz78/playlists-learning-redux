1333136cfdac8c00592ae172913ff144798fbc12
Md Abu Zafar <mdabuzafar78@gmail.com>
2024-05-27 11:45:20 +0530

    setup store

---
+++ src/store/index.js
@@ -0,0 +1,30 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+const songsSlice = createSlice({
+  name: "song",
+  initialState: [],
+  reducers: {
+    addSong(state, action) {
+      state.push(action.payload);
+    },
+    removeSong(state, action) {},
+  },
+});
+
+const store = configureStore({
+  reducer: {
+    songs: songsSlice.reducer,
+  },
+});
+
+// const initialState = store.getState();
+
+// store.dispatch({
+//   type: "song/addSong",
+//   payload: "New Song",
+// });
+
+// const finalState = store.getState();
+// console.log({ initialState, finalState });
+
+export { store };
